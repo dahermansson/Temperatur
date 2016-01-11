8d6fb8996b9cff51237bbedea32d353abdacd994
Hermansson Daniel <daniel.hermansson@trafikverket.se>
2016-01-11 16:21:09 +0100

    First

---
+++ app.ts
@@ -0,0 +1,13 @@
+import express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res){
+    res.send('<h1>Hello World!!</h1>');
+})
+
+app.listen(3000, function(){
+    console.log("Lyssnar");
+})
+
+export = app;
